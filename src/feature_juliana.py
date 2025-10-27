e71476dbc0c480527c821a03b8dc8dcbc4d2b64d
Bryan Felipe Packer <115124886+BryanPacker@users.noreply.github.com>
2025-10-27 20:29:24 -0300

    Revert "feature da juliana (#1)"
    This reverts commit a97ef99dbbce655f726e1e08791faabf3776b5d9.

--- src/feature_juliana.py
+++
@@ -1,9 +0,0 @@
-def ra(a, b):
-    """
-    Feature RA - Subtração
-    Retorna o resultado da subtração entre dois números.
-    """
-    try:
-        return a - b
-    except TypeError:
-        raise ValueError("Os parâmetros devem ser números.")
