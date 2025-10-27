01a9453b62e7f92e67a846f8db1fe1a9bbf0c405
Giovani L. Rozza <giovanni.rozza@gmail.com>
2025-10-27 20:34:28 -0300

    Implementa função para multiplicar tres números

---
+++ multiplica-tres-numeros.py
@@ -0,0 +1,13 @@
+# Programa para multiplicar tres números
+
+# Lê os dois números do usuário
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+numero3 = float(input("Digite o terceiro número: "))
+
+# Calcula o produto
+resultado = numero1 * numero2*numero2
+
+# Mostra o resultado
+print(f"O resultado da multiplicação é: {resultado}")
+
